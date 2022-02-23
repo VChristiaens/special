04dadaa7461566f1fe3b4ad92b3dae354026636b
VChristiaens <valentinchrist@hotmail.com>
2022-02-23 17:42:02 +0100

    Bug fix in helper function to write docs from README

--- helpers/update_docs_rst_from_README.py
+++ helpers/update_docs_rst_from_README.py
@@ -4,20 +4,16 @@ Update intro text in documentation using text in README.rst
 """
 
 
-import nbformat
-import re
-
-
-with open("../README.rst") as f_r:
-    readme = f_r.read()
+with open("README.rst") as f_r:
+    readme = f_r.readlines()
 
 start_write=False
 stop_write=False
 str_start = 'TL;DR'
-str_stop = 'About special'
-with open("../docs/trimmed_readme.rst", 'w') as f_i:
+str_stop = 'About `special'
+with open("docs/trimmed_readme.rst", 'w') as f_i:
     for l, line in enumerate(readme):
-        if start_write and not stop_write:
+        if start_write and not str_stop in line and not stop_write:
             f_i.write(line)
         elif str_start in line and not stop_write:
             f_i.write(line)
@@ -25,11 +21,15 @@ with open("../docs/trimmed_readme.rst", 'w') as f_i:
         elif str_stop in line:
             stop_write=True
             l_about = l
+        else:
+            continue
             
-with open("../docs/about.rst", 'w') as f_i:
+with open("docs/about.rst", 'w') as f_i:
     for l, line in enumerate(readme):
-        if l >= l_about:
+        if l > l_about+1:
+            line.replace('^', '-')
             f_i.write(line)
+            
             
 
 print("successfully converted README into documentation rst files.")
